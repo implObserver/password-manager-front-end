edc4f234c563dda4e2d73cb242dd3e709a5770f2
implObserver <prognozman@mail.ru>
2024-12-02 20:05:02 +0500

    feat:add base reducers for passwords

--- src/services/passKeeper/entities/password/model/slice/slice.ts
+++ src/services/passKeeper/entities/password/model/slice/slice.ts
@@ -1,10 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { initialState } from "./defaultState";
 
 const passwordsSlice = createSlice({
     name: 'passwords',
     initialState,
-    reducers: {},
+    reducers: {
+        addPassword: (state: Pair[], action: PayloadAction<Pair>) => {
+            const index = state.findIndex(pair => pair.service === action.payload.service);
+            if (index !== -1) {
+                state.push(action.payload);
+            }
+        },
+        removePassword: (state: Pair[], action: PayloadAction<Pair>) => {
+            const index = state.findIndex(pair => pair.service === action.payload.service);
+            if (index !== -1) {
+                state.splice(index, 1);
+            }
+        },
+        updatePassword: (state: Pair[], action: PayloadAction<Pair>) => {
+            const index = state.findIndex(pair => pair.service === action.payload.service);
+            if (index !== -1) {
+                state.splice(index, 1, action.payload);
+            }
+        }
+    },
     extraReducers: (builder) => { },
 })
 
