2c4073d2c2fa329e11fd249730780a72a2e63469
implObserver <prognozman@mail.ru>
2024-12-03 22:55:16 +0500

    fix: card of service

--- src/services/passKeeper/entities/bigPasswordInput/ui/BigPasswordInput.tsx
+++ src/services/passKeeper/entities/bigPasswordInput/ui/BigPasswordInput.tsx
@@ -3,7 +3,11 @@ import styles from './styles/BigPasswordInput.module.css'
 
 export const BigPasswordInput = ({ lock }) => {
     return (
-        <div className={styles.input}>
+        <div className={`
+            ${styles.input} 
+            ${lock
+                ? styles.lock
+                : ''}`}>
             <PasswordInput lock={lock}></PasswordInput>
         </div>
     )
--- src/services/passKeeper/entities/bigServiceInput/ui/BigServiceInput.tsx
+++ src/services/passKeeper/entities/bigServiceInput/ui/BigServiceInput.tsx
@@ -1,9 +1,13 @@
 import { ServiceInput } from "@/services/passKeeper/shared/ui/serviceInput";
 import styles from './styles/BigServiceInput.module.css'
 
-export const BigServiceInput = ({lock}) => {
+export const BigServiceInput = ({ lock }) => {
     return (
-        <div className={styles.input}>
+        <div className={`
+        ${styles.input} 
+        ${lock
+                ? styles.lock
+                : ''}`}>
             <ServiceInput lock={lock}></ServiceInput>
         </div>
     )
