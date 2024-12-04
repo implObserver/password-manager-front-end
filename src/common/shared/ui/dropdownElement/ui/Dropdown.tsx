0e0167c1ce68b55a7c63b34cb570578dfe349a56
implObserver <prognozman@mail.ru>
2024-12-04 10:21:57 +0500

    ref:delete dropdown smooth effect

--- src/common/shared/ui/dropdownElement/ui/Dropdown.tsx
+++ src/common/shared/ui/dropdownElement/ui/Dropdown.tsx
@@ -1,22 +1,11 @@
 import styles from './styles/Dropdown.module.css'
 import { useDropdownContext } from '../lib/context/Context.';
-import { useEffect, useRef } from 'react';
 
 export const Dropdown = ({ children }) => {
     const status = useDropdownContext();
 
-    const endOfListRef = useRef<HTMLDivElement | null>(null);
-
-    useEffect(() => {
-        if (endOfListRef.current) {
-            setTimeout(() => {
-                endOfListRef.current.scrollIntoView({ behavior: "smooth" });
-            }, 150);
-        }
-    }, [status]);
-
     return (
-        <div ref={endOfListRef} className={styles.dropdown}>
+        <div className={styles.dropdown}>
             <div className={`
                 ${styles.content} 
                 ${status.state ? styles.open : ''}
