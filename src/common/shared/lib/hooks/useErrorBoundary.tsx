434663d45aab147cc79847a30f1193d30f59ba0c
implObserver <prognozman@mail.ru>
2024-12-05 14:53:46 +0500

    ref:error boundary

--- src/common/shared/lib/hooks/useErrorBoundary.tsx
+++ src/common/shared/lib/hooks/useErrorBoundary.tsx
@@ -1,35 +1,37 @@
-import React, { useState, useEffect, ErrorInfo } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// Определяем тип для пропсов
 interface Props {
   children: React.ReactNode;
 }
 
 export const ErrorBoundary: React.FC<Props> = ({ children }) => {
   const [hasError, setHasError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const handleError = (error: Error) => {
     console.error('Error caught by ErrorBoundary:', error);
+    // Устанавливаем текст ошибки, который будет отображаться в h1
+    setErrorMessage(error.message);
   };
 
   useEffect(() => {
-    const errorHandler = (error: Error) => {
+    const errorHandler = (event: ErrorEvent) => {
       setHasError(true);
-      handleError(error);
+      handleError(event.error);
     };
 
     // Добавляем слушателя для обработки ошибок
-    window.addEventListener('error', (event) => errorHandler(event.error));
+    window.addEventListener('error', errorHandler);
 
     // Удаляем слушателя при размонтировании
     return () => {
-      window.removeEventListener('error', (event) => errorHandler(event.error));
+      window.removeEventListener('error', errorHandler);
     };
   }, []);
 
   if (hasError) {
     // Выводим запасной интерфейс при возникновении ошибки
-    return <h1>Что-то пошло не так.</h1>;
+    return <h1>{errorMessage || 'Что-то пошло не так.'}</h1>;
   }
 
   return <>{children}</>;
